376e02d1c22a8802b033ae62f86207770cd62a98
Omid Beheshtian <beheshtian.omid82@gmail.com>
2021-07-18 03:14:54 +0430

    Update index.php

--- index.php
+++ index.php
@@ -19,77 +19,105 @@ if(isset($_GET['send'])){
     <title>Document</title>
 </head>
 <body>
-        <form action="" method="GET">
-            <div id="maghsad">
-                <input type="radio" id="m1" name="maghsad" value="euro">
-                <label for="m1">اروپا</label>
-                <input type="radio" id="m2" name="maghsad" value="canada">
-                <label for="m2">کانادا</label>
+<form action="" method="GET">
+        <div id="maghsad">
+            <p class="omidbeheshtianp">مقصد مهاجرت</p>
+            <input type="radio" id="m1" name="maghsad" value="euro">
+            <label for="m1">اروپا</label>
+            <input type="radio" id="m2" name="maghsad" value="canada">
+            <label for="m2">کانادا</label>
+        </div>
+        <div id="age">
+        <p class="omidbeheshtianp">بازه سنی:</p>
+        <select class="input100" name="age">
+                <option value="18-23">18 تا 23 سال</option>
+                <option value="23-35">23 تا 35 سال</option>
+                <option value="35-50">35 تا 50 سال</option>
+                <option value="50+">بالای 50 سال</option>
+            </select>
+        </div>
+        <div id="job">
+            <p class="omidbeheshtianp">شغل:</p>
+            <select class="input100" name="job">
+                <option value="karmand">کارمند</option>
+                <option value="sahebkar">صاحب کسب و کار</option>
+                <option value="azad">شغل آزاد</option>
+                <option value="adameshteghal">عدم اشتغال</option>
+            </select>
+        </div>
+        <div id="tamakonmali">
+            <p class="omidbeheshtianp">تمکن مالی:</p>
+            <select class="input100" name="tamakonmali">
+                <option value="200-500">200 میلیون تا 500 میلیون</option>
+                <option value="500-1">500 میلیون تا 1 میلیارد</option>
+                <option value="1-2">1 میلیارد تا 2 میلیارد</option>
+                <option value="2+">2 میلیارد به بالا</option>
+            </select>
+        </div>
+        <div id="madrak">
+            <p class="omidbeheshtianp">مدرک زبان انگلیسی:</p>
+            <input type="radio" id="z1" name="madrak" value="y">
+            <label for="z1">دارم</label>
+            <input type="radio" id="z2" name="madrak" value="n">
+            <label for="z2">ندارم</label>
+        </div>
+        <div id="madrakgheyr">
+            <p class="omidbeheshtianp">مدرک زبان غیر از انگلیسی:</p>
+            <input type="radio" id="mg1" name="madrakgheyr" value="y">
+            <label for="mg1">دارم</label>
+            <input type="radio" id="mg2" name="madrakgheyr" value="n">
+            <label for="mg2">ندارم</label>
+        </div>
+        <div id="madraktahsili">
+            <p class="omidbeheshtianp">مدرک تحصیلی:</p>
+            <select class="input100" name="madraktahsili">
+                <option value="n">ندارم</option>
+                <option value="diplom">دیپلم</option>
+                <option value="karshenasi">کارشناسی</option>
+                <option value="karshenasiarshad">کارشناسی ارشد</option>
+                <option value="doctor">دکتری</option>
+            </select>
+        </div>
+        <div id="sabeghesafar">
+            <p class="omidbeheshtianp">سابقه سفری:</p>
+            <input type="radio" id="sf1" name="sabeghesafar" value="y">
+            <label for="sf1">دارم</label>
+            <input type="radio" id="sf2" name="sabeghesafar" value="n">
+            <label for="sf2">ندارم</label>
+        </div>
+        <div id="taahol">
+            <p class="omidbeheshtianp">وضغیت تاهل</p>
+            <input type="radio" id="t1" name="taahol" value="motahel" onclick="farzand()">
+            <label for="t1">متاهل</label>
+            <input type="radio" id="t2" name="taahol" value="mojarad" onclick="farzandhide()">
+            <label for="t2">مجرد</label>
+            <div id="2tt" style="display:none;">
+            <p class="omidbeheshtianp">فرزند:</p>
+            <input type="radio" id="yc" name="child" value="yes">
+                <label for="yc">دارم</label>
+                <input type="radio" id="nc" name="child" value="yes">
+                <label for="nc">ندارم</label>
             </div>
-            <div id="age">
-            <select name="age">
-                    <option value="18-23">18 تا 23 سال</option>
-                    <option value="23-35">23 تا 35 سال</option>
-                    <option value="35-50">35 تا 50 سال</option>
-                    <option value="50+">بالای 50 سال</option>
-                </select>
-            </div>
-            <div id="job">
-                <select name="job">
-                    <option value="karmand">کارمند</option>
-                    <option value="sahebkar">صاحب کسب و کار</option>
-                    <option value="azad">شغل آزاد</option>
-                    <option value="adameshteghal">عدم اشتغال</option>
-                </select>
-            </div>
-            <div id="tamakonmali">
-                <select name="tamakonmali">
-                    <option value="200-500">200 میلیون تا 500 میلیون</option>
-                    <option value="500-1">500 میلیون تا 1 میلیارد</option>
-                    <option value="1-2">1 میلیارد تا 2 میلیارد</option>
-                    <option value="2+">2 میلیارد به بالا</option>
-                </select>
-            </div>
-            <div id="madrak">
-                <input type="radio" id="z1" name="madrak" value="y">
-                <label for="z1">دارم</label>
-                <input type="radio" id="z2" name="madrak" value="n">
-                <label for="z2">ندارم</label>
-            </div>
-            <div id="madrakgheyr">
-                <input type="radio" id="mg1" name="madrakgheyr" value="y">
-                <label for="mg1">دارم</label>
-                <input type="radio" id="mg2" name="madrakgheyr" value="n">
-                <label for="mg2">ندارم</label>
-            </div>
-            <div id="madraktahsili">
-                <select name="madraktahsili">
-                    <option value="n">ندارم</option>
-                    <option value="diplom">دیپلم</option>
-                    <option value="karshenasi">کارشناسی</option>
-                    <option value="karshenasiarshad">کارشناسی ارشد</option>
-                    <option value="doctor">دکتری</option>
-                </select>
-            </div>
-            <div id="sabeghesafar">
-                <input type="radio" id="sf1" name="sabeghesafar" value="y">
-                <label for="sf1">دارم</label>
-                <input type="radio" id="sf2" name="sabeghesafar" value="n">
-                <label for="sf2">ندارم</label>
-            </div>
-            <div id="taahol">
-                <input type="radio" id="t1" name="taahol" value="motahel">
-                <label for="t1">متاهل</label>
-                <input type="radio" id="t2" name="taahol" value="mojarad">
-                <label for="t2">مجرد</label>
-            </div>
-            <div id="akhzvisa">
-                <input type="radio" id="av1" name="akhzvisa" value="y">
-                <label for="av1">دارم</label>
-                <input type="radio" id="av2" name="akhzvisa" value="n">
-                <label for="av2">ندارم</label>
-            </div>
-            <input type="submit" value="ارسال" name="send">
-        </form>
+        </div>
+        <div id="akhzvisa">
+            <p class="omidbeheshtianp">سابقه اقدام جهت اخذ هر نوع ویزا یا اقامت:</p>
+            <input type="radio" id="av1" name="akhzvisa" value="y">
+            <label for="av1">دارم</label>
+            <input type="radio" id="av2" name="akhzvisa" value="n">
+            <label for="av2">ندارم</label>
+        </div>
+    </div>
+    <div>
+        <button class="login100-form-btn" name="send">ادامه</button>
+    </div>
+</form>
+<script>
+function farzand() {
+  document.getElementById("2tt").style.display = "block";
+}
+function farzandhide() {
+    document.getElementById("2tt").style.display = "none";
+}
+</script>
 </body>
 </html>
